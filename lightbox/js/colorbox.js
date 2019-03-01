bdda9fce13a4eb271f5dfd6635079c6c28e6a395
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2019-03-01 16:01:51 -0500

    Test de cadre css 3 en marie-louise hardcodé en js pour #3

--- lightbox/js/colorbox.js
+++ lightbox/js/colorbox.js
@@ -125,11 +125,16 @@
       left: parseInt(this.$image.css('border-left-width'), 10)
     };
 
-	// DEBUT - Maintenance par Nadine Giasson St-Amand - 1er mars 2019
+	// DEBUT - Maintenance par Nadine Giasson St-Amand - 1er mars 2019 - ticket #2
 	this.$outerContainer.css('border-radius',this.options.roundCorner);
 	this.$container.css('border-radius',this.options.roundCorner);
 	this.$image.css('border-radius',this.options.roundCorner);
-	// FIN - Maintenance par Nadine Giasson St-Amand - 1er mars 2019
+	// FIN - Maintenance par Nadine Giasson St-Amand - 1er mars 2019 - ticket #2
+	
+	// DEBUT - Maintenance par Nadine Giasson St-Amand - 1er mars 2019 - ticket #3
+	this.$outerContainer.css('padding','100px');
+	this.$outerContainer.addClass('honeycomb');
+	// FIN - Maintenance par Nadine Giasson St-Amand - 1er mars 2019 - ticket #3
 	
     // Attach event handlers to the newly minted DOM elements
     this.$overlay.hide().on('click', function() {
